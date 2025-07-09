a4960f1bffb0a7ca73d088e2233c11384df26084
Dmitriy Fishman <fishman.code@gmail.com>
2025-07-09 07:25:46 +0300

    Updated User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'dob',
+        'is_active',
+        'level',
+        'rating',
+        'metadata',
     ];
 
     /**
@@ -43,6 +48,11 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'dob' => 'date',
+            'is_active' => 'boolean',
+            'level' => 'integer',
+            'rating' => 'float',
+            'metadata' => 'json',
         ];
     }
 }
