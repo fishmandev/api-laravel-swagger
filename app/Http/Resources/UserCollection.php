0ebc949d54c45fca72bc809d966182c872c02bb3
Dmitriy Fishman <fishman.code@gmail.com>
2025-07-09 11:41:54 +0300

    Updated a UserCollection

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -16,27 +16,9 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class UserCollection extends ResourceCollection
 {
     /**
-     * Transform the paginated user collection into an array
-     * 
-     * Converts a paginated collection of User models into a standardized
-     * array structure with data and pagination metadata. Each user is
-     * transformed using the UserResource class.
-     * 
-     * @param Request $request The HTTP request instance
-     * @return array<int|string, mixed> Standardized collection response
+     * The resource that this resource collects.
+     *
+     * @var string
      */
-    public function toArray(Request $request): array
-    {
-        return [
-            'data' => UserResource::collection($this->collection),
-            'meta' => [
-                'total' => $this->total(),
-                'count' => $this->count(),
-                'per_page' => $this->perPage(),
-                'current_page' => $this->currentPage(),
-                'total_pages' => $this->lastPage(),
-                'has_more_pages' => $this->hasMorePages(),
-            ],
-        ];
-    }
+    public $collects = UserResource::class;
 }
